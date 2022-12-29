c8f0f31bfc10ee4494a9938d518adfb49314021a
Vova <kazap3r@gmail.com>
2022-12-29 11:05:27 +0200

    added a way to choose prefered way of comunication for checkout

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -19,7 +19,7 @@ const Checkout = () => {
     const [showForm,setShowForm] = useState(false)
     const [name,setName] = useState('')
     const [phone,setPhone] = useState('')
-
+    const [communication,setCommunication] = useState<'phone'|'telegram'>('phone')
 
     
     const cart = useAppSelector(state => state.cart)
@@ -30,6 +30,7 @@ const Checkout = () => {
         Надійшло нове замовлення!\n
         Ім'я: ${name}\n
         Телефон: ${phone}\n
+        Спосіб зв'язку: ${communication==='phone'?'Телефон' :'Telegram'}\n
         `
 
         cart.forEach(item => {
@@ -66,7 +67,8 @@ const Checkout = () => {
                 </div>
 
             </section>
-           {showForm && <CheckoutForm setShowForm={setShowForm} name={name} setName={setName} phone={phone} setPhone={setPhone} handleCheckout={handleCheckout}/>}
+           {showForm && <CheckoutForm setShowForm={setShowForm} name={name} setName={setName} phone={phone} setPhone={setPhone} handleCheckout={handleCheckout}
+            setCommunication={setCommunication}/>}
         </>
      );
 }
@@ -81,10 +83,18 @@ interface CheckoutFormProps {
     phone: string
     setPhone: (value: string) => void
     handleCheckout: (e:React.FormEvent<HTMLFormElement>) => void
+    setCommunication: (value: 'phone'|'telegram') => void
 }
 
 
-const CheckoutForm:FC<CheckoutFormProps> = ({setShowForm,name,setName,phone,setPhone,handleCheckout}) => {
+const CheckoutForm:FC<CheckoutFormProps> = ({setShowForm,name,setName,phone,setPhone,handleCheckout,setCommunication}) => {
+    
+    const handleChange = (e:React.ChangeEvent<HTMLSelectElement>) => {
+        setCommunication(e.target.value as 'phone'|'telegram')
+    }
+    
+    
+    
     return(
         <div className="w-screen h-screen bg-slate-800 absolute top-0 left-0 z-50 flex flex-col justify-center items-center gap-5">
             <button onClick={()=>setShowForm(false)}className='text-white text-4xl font-bold'>X</button>
@@ -98,6 +108,16 @@ const CheckoutForm:FC<CheckoutFormProps> = ({setShowForm,name,setName,phone,setP
                     <label className="text-white mb-1 ">Номер телефону</label>
                     <input type="text" placeholder="099-333-33-33" value={phone} onChange={(e)=>setPhone(e.target.value)} required/>
                 </div>
+
+                <div className="w-full flex flex-col justify-center mb-5">
+                    <span className="text-white mb-1">Оберіть спосіб зв'язку</span>
+                    <select onChange={handleChange}>
+                        <option value="phone">Телефон</option>
+                        <option value="telegram">Telegram</option>
+                    </select>
+                </div>
+
+           
 
                 <ButtonRounded text='Оформити замовлення' color='bg-teal-400'/>
             </form>
